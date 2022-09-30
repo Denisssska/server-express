87914ac95542a2b8076b5faffba1b6a16ebf1095
unknown <yarmoshkoden18m@gmail.com>
2022-09-30 21:20:24 +0300

    make it better

--- db.js
+++ db.js
@@ -4,6 +4,11 @@ module.exports = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD, {
         dialect:'postgres',
+        pool: {   // Настройки пула подключений
+            max: 5, // Максимальное количество подключений
+            min: 0, // Минимальное количество подключений
+            idle: 10000
+        },
         host:process.env.DB_HOST,
         port:process.env.DB_PORT
     }
