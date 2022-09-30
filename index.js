ad5f07b9cf095d5f34e81172b92792a648da374b
unknown <yarmoshkoden18m@gmail.com>
2022-09-30 21:39:12 +0300

    make it better

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 const start = async () => {
     try {
-        // await sequelize.authenticate()
+         await sequelize.authenticate()
         // await sequelize.sync()
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
