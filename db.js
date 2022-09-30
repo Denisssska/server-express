f070a40350ba81098ee89ec313962bac20cec961
unknown <yarmoshkoden18m@gmail.com>
2022-09-30 20:55:00 +0300

    make it better

--- db.js
+++ db.js
@@ -1,31 +1,11 @@
 const {Sequelize} = require('sequelize')
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        dialect: 'postgres',
-        host: process.env.DB_HOST ,
-        port: process.env.DB_PORT
-    }
-)
-// let sequelize;
-// if (process.env.NODE_ENV === "production") {
-//     sequelize = new Sequelize(process.env.DATABASE_URL);
-// } else {
-//     sequelize = new Sequelize(
-//         process.env.POSTGRES_DB || "elitypescript",
-//         process.env.POSTGRES_USER || "eli",
-//         "",
-//         {
-//             host: process.env.PSQL_HOST || "localhost",
-//             dialect: "postgres",
-//             pool: {
-//                 max: 100,
-//                 min: 0,
-//                 idle: 200000,
-//                 // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
-//                 acquire: 1000000,
-//             },
-//         }
-//     );
-// }
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD, {
+//         dialect:'postgres',
+//         host:process.env.DB_HOST,
+//         port:process.env.DB_PORT
+//     }
+// )
+module.exports = new Sequelize('postgres://user:postgres:5432/postgres')
